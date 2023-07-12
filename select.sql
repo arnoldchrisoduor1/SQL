12e58e4d95959ff170dedc28dbef2baf15f77583
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-07-12 12:06:41 +0300

    Select statement

--- select.sql
+++ select.sql
@@ -2,3 +2,14 @@
 
 SELECT column1, column2
 FROM table_name;
+
+/*Ignoring all the duplicate items.*/
+SELECT DISTINCT Country FROM Customers;
+
+/*Counting all the distinct values in the database*/
+SELECT COUNT(DISTINCT Country) FROM Customers;
+/*This is not possible in MS Access databases.*/
+
+/*A work around the problem is.*/
+SELECT Count(*) AS DistinctCountries
+FROM(SELECT DISTINCT Country FROM Customers);
