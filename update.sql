1b090bf44862f1c6e3a9f44f66835541d000b40e
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-07-15 10:10:01 +0300

    update sql

--- update.sql
+++ update.sql
@@ -1,1 +1,14 @@
 /*Used to modify the existing data in the table.*/
+UPDATE table_name
+SET column1 = value1, column2 = value2
+WHERE condition;
+
+/*Example on a table*/
+UPDATE customer_table
+SET CustomerName = 'Alan Turing', customer_address = '345 park avenue'
+WHERE CustomerID = 12;
+
+/*Updating multiple records.*/
+UPDATE Customers_table
+SET CustomerName = 'Juan'
+WHERE Country = 'Mexico';
--- where.sql
+++ where.sql
@@ -6,39 +6,39 @@ WHERE condition;
 
 /*WHERE can also be used in DELETE, UPDATE commands*/
 SELECT * FROM Customers
-WHERE Country = 'Mexico'
+WHERE Country = 'Mexico';
 
 /*When identifying numeric values*/
 SELECT * FROM Customers
-WHERE CustomerID = 1
+WHERE CustomerID = 1;
 
 /*We can also use the terms BETWEEN, LIKE and 
 IN to get any value we required.*/
 
 /*Using the NOT keyword.*/
 SELECT * FROM Customers
-WHERE NOT City = 'Berlin'
+WHERE NOT City = 'Berlin';
 
 /*Using the AND statement*/
 SELECT * FROM Customers
 WHERE CutomerID = 56
-AND PostalAddress > 46
+AND PostalAddress > 46;
 
 /*Using the OR keyword*/
 SELECT * FROM Customers
 WHERE CustomerID = 78
-OR CustomerID = 98
+OR CustomerID = 98;
 
 /*even more and*/
 SELECT * FROM Customers
-WHERE CustomerID = 76 AND City = 'Berlin'
+WHERE CustomerID = 76 AND City = 'Berlin';
 
 /*Combing or and and*/
 SELECT * FROM Customers
-WHERE Country = 'Germany' AND (City = 'Berlin' OR City = 'Muchen')
+WHERE Country = 'Germany' AND (City = 'Berlin' OR City = 'Muchen');
 
 
 /*Combining and and not*/
 SELECT * FROM Customers
-WHERE NOT Country = 'America' AND NOT Country = 'Europe'
+WHERE NOT Country = 'America' AND NOT Country = 'Europe';
 
