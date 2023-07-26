2170ac6d79bfe655a4ebe41e0286e646a5e7c889
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-07-26 20:00:28 +0300

    left join

---
+++ leftjoin.sql
@@ -0,0 +1,9 @@
+/*LEFT JOIN keyword returns all the contents of the
+the left table and none if the right has no match.*/
+
+/*LEFT JOIN - LEFT OUTER JOIN*/
+
+SELECT Customers.CustomerName, Orders.OrderID
+FROM Customers
+LEFT JOIN ON Customers.CustomerID = Orders.OrderID
+ORDER BY Customers.CustomerName;
